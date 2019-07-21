23c85309ca43b32ffa56275b9e34e8cf568e50e1
hortmagen21 <max_yudkin@ukr.net>
2019-07-21 19:20:32 +0300

    no message

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from http.server import HTTPServer
 from ServerRuner.server import Server
 HOST_NAME='localhost'
 PORT_NUMBER=int(sys.argv[1])
+print(PORT_NUMBER)
 #if __name__ == '__main__':#убеждаемся что ето именно файл main
 httpd = HTTPServer((HOST_NAME, PORT_NUMBER), Server)#оздаем обьект сервера
 print(time.asctime(), 'Server UP - %s:%s' % (HOST_NAME, PORT_NUMBER))
