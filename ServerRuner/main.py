3cd807c9ef51849ff809650d770da166619ea409
hortmagen21 <max_yudkin@ukr.net>
2019-07-24 21:11:08 +0300

    no message

--- ServerRuner/main.py
+++ ServerRuner/main.py
@@ -2,7 +2,7 @@ import sys
 import time
 from http.server import HTTPServer
 from server import Server
-HOST_NAME='localhost'
+HOST_NAME=''
 PORT_NUMBER=int(sys.argv[1])
 
 if __name__ == '__main__':#убеждаемся что ето именно файл main
