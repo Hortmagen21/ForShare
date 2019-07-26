c77dfaf2be38f5b2e81b0fa80088cb077ec1a02a
hortmagen21 <max_yudkin@ukr.net>
2019-07-26 16:11:37 +0300

    open false to open true which is better

--- ServerRuner/response/public/mainButtons.js
+++ ServerRuner/response/public/mainButtons.js
@@ -1,27 +1,37 @@
 $('document').ready(function(){
 $('#enter').on('click',function(){
+    var enterbtn;
+    enterbtn=document.getElementById("enter");//взяли кнопку с айди enter
     var Loger;
     var Loger=new XMLHttpRequest();
-    Loger.open('GET','database',false);
+    Loger.open('GET','database',true);//database путь в router
     Loger.send();
+    Loger.onreadystatechange=function(){
+    if (Loger.readyState !=4)return;
     if (Loger.status !=200){
-	    location.href="/login";}
+        alert(Loger.status)
+	    location.href="/login";
+	    enterbtn.disabled=false;
+	    }
     else{
 	    var jsoncontain;
-	    var jsoncontain=JSON.parse(Loger.responseText);
+	    var jsoncontain=JSON.parse(Loger.responseText);//храним переработаный JSON
 	    //alert(jsoncontain);
 	    //alert(Loger.responseText);
 	    //alert(jsoncontain.username);
         var username;
         var password;
-        username=jQuery('#username').val();
+        username=jQuery('#username').val();//берем переменные с полей
         password=jQuery('#password').val();
-        alert(password);
-        alert(username);
+        //alert(password);
+        //alert(username);
         if (jsoncontain.username==username && jsoncontain.password==password){
         location.href="/";}
         else{
-        alert('error');}
+        alert('wrong password or username');
+        enterbtn.disabled=false;
+        }
         }}
-);
+        enterbtn.disabled=true;
+});
 });
--- ServerRuner/response/requestHandler.py
+++ ServerRuner/response/requestHandler.py
@@ -1,19 +1,27 @@
-class MockFile():#если ми не сможем прочитать файл обычним read() то видаст False
+class MockFile():  # если ми не сможем прочитать файл обычним read() то видаст False
     def read(self):
         return False
+
+
 class RequestHandler():
-    def __init__(self):#создали конструктор
-        self.contentType=""#создавая обьект у нас есть 2 параметра contentType,contents
-        self.contents=MockFile()
-    def getContents(self):#если нужно взять contents
+    def __init__(self):  # создали конструктор
+        self.contentType = ""  # создавая обьект у нас есть 2 параметра contentType,contents
+        self.contents = MockFile()
+
+    def getContents(self):  # если нужно взять contents
         return self.contents.read()
-    def read(self):#просмотр файла врод
+
+    def read(self):  # просмотр файла врод
         return self.contents
-    def setStatus(self,status):#код успеха поставить
-        self.status=status
-    def getStatus(self):#взять
+
+    def setStatus(self, status):  # код успеха поставить
+        self.status = status
+
+    def getStatus(self):  # взять
         return self.status
-    def getContentType(self):#взять тип контента
+
+    def getContentType(self):  # взять тип контента
         return self.contentType
+
     def getType(self):
         return "static"
