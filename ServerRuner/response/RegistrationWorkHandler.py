e5542c63d6a11d8026c29764ec5a1ef2978c69f7
hortmagen21 <max_yudkin@ukr.net>
2019-08-14 19:06:21 +0300

    setStatus RegistWork

--- ServerRuner/response/RegistrationWorkHandler.py
+++ ServerRuner/response/RegistrationWorkHandler.py
@@ -33,7 +33,12 @@ class RegistrationHandler(RequestHandler):
     def checkSQLData(self,data):
         database=psycopg2.connect("dbname=d7f6m0it9u59pk user=iffjnrmpbopayf host=ec2-54-83-1-101.compute-1.amazonaws.com password=20d31f747b4397c839a05d6d70d2decd02b23a689d86773a84d8dcfa23428946 port=5432")
         cursor=database.cursor()
-        cursor.execute("INSERT INTO AccountCreate (id,name,password) VALUES(%s,%s, %s)", (1,data["username"], data["password"]))
+        self.setStatus(200)
+        try:
+            cursor.execute("INSERT INTO AccountCreate (id,name,password) VALUES(%s,%s, %s)", (1,data["username"], data["password"]))
+        except:
+            self.setStatus(409)
+
         #cursor.execute("SELECT * FROM test;")
         #cursor.fetchone()
         #(1,data["username"], data["password"])
